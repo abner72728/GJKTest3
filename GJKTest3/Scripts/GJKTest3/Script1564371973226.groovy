8cc6cdc126ad8d5fed360144b5066968ce59556c
abnertang <b9009008@chu.edu.tw>
2019-07-29 11:49:43 +0800

    Crete New Script GJKTest3 : change sciprt payj

---
+++ GJKTest3/Scripts/GJKTest3/Script1564371973226.groovy
@@ -0,0 +1,36 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://katalon-demo-cura.herokuapp.com/')
+
+WebUI.click(findTestObject('Object Repository/Demo_Obj/Page_LoginLogout/a_Make Appointment'))
+
+WebUI.setText(findTestObject('Object Repository/Demo_Obj/Page_LoginLogout/input_Username_username'), 'John Doe')
+
+WebUI.setText(findTestObject('Demo_Obj/Page_LoginLogout/input_Password_password'), 'ThisIsNotAPassword')
+
+WebUI.click(findTestObject('Object Repository/Demo_Obj/Page_LoginLogout/button_Login'))
+
+WebUI.delay(3)
+
+//println("Login Success")
+
+WebUI.click(findTestObject('Object Repository/Demo_Obj/Page_LoginLogout/i_CURA Healthcare_fa fa-bars'))
+
+WebUI.click(findTestObject('Object Repository/Demo_Obj/Page_LoginLogout/a_Logout'))
+
+WebUI.closeBrowser()
---
+++ GJKTest3/Test Suites/Demo GJK Test3.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Demo GJK Test3</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>d473bf34-ffa9-4d6d-801c-650cf7a12c76</testSuiteGuid>
+   <testCaseLink>
+      <guid>46f4e858-f2d2-414b-b3b2-40b275ae540a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/GJKTest3</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
